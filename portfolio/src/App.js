e428116e13d5e1a25b0d626a137444b06c20c3bb
MSI <75710210+doug-e-z@users.noreply.github.com>
2022-02-14 23:38:02 -0500

    added new components and started working on header

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,10 +1,10 @@
-import Header from './components/Header'
+import { Container, Row, Col } from "react-bootstrap/";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello from React</h1>
-        <Header/>      
+      <Header />
     </div>
   );
 }
--- portfolio/src/components/Header.js
+++ portfolio/src/components/Header.js
@@ -1,9 +1,19 @@
+import { Container, Row, Col, Image } from "react-bootstrap/";
+import profile from "../static/images/3AQo1xFw.jpeg";
+import "../static/css/header.css";
+
 const Header = () => {
   return (
     <header>
-        <h1>Hello from header</h1>
+      <Container>
+        <Row className="row">
+          <Col>
+            <Image src={profile} className="profile" alt="me"></Image>
+          </Col>
+        </Row>
+      </Container>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
