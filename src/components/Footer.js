f5ac9df25a5f61b6b500d8d3d3c2bd269d1d3a99
MSI <75710210+doug-e-z@users.noreply.github.com>
2022-02-16 22:31:41 -0500

    Fixed highlighting feature

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,12 +7,15 @@ const Footer = () => {
       <Container>
         <Row className="footer-row">
           <Navbar className="navbar navbar-expand-xl navbar-dark">
-            <Nav className="me-auto">
+            <Nav className="me-auto footer-nav">
               <Nav.Link
-                className="fa fa-linkedin-square"
+                className="fa fa-linkedin-square footer-link"
                 href="https://linkedin.com/in/doug-z-851285111"
               ></Nav.Link>
-              <Nav.Link className="fa fa-github" href="https://github.com/doug-e-z"></Nav.Link>
+              <Nav.Link
+                className="fa fa-github footer-link"
+                href="https://github.com/doug-e-z"
+              ></Nav.Link>
             </Nav>
           </Navbar>
         </Row>
