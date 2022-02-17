b90d4b0ea216b2c52069987159716e9dae00b9f5
MSI <75710210+doug-e-z@users.noreply.github.com>
2022-02-16 20:44:42 -0500

    acquired icons for media site share

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,18 +1,24 @@
-import {Container, Row, Col} from "react-bootstrap/";
+import { Container, Row, Navbar, Nav } from "react-bootstrap/";
 import "../static/css/footer.css";
 
 const Footer = () => {
-    return (
-      <div>
-        <Container>
-          <Row className="footer-row">
-            <Col>
-             <p>Footer goes here</p>
-            </Col>
-          </Row>
-        </Container>
-      </div>
-    );
-  };
-  
-  export default Footer;
+  return (
+    <div>
+      <Container>
+        <Row className="footer-row">
+          <Navbar className="navbar navbar-expand-xl navbar-dark">
+            <Nav className="me-auto">
+              <Nav.Link
+                className="fa fa-linkedin-square"
+                href="https://linkedin.com/in/doug-z-851285111"
+              ></Nav.Link>
+              <Nav.Link className="fa fa-github" href="https://github.com/doug-e-z"></Nav.Link>
+            </Nav>
+          </Navbar>
+        </Row>
+      </Container>
+    </div>
+  );
+};
+
+export default Footer;
