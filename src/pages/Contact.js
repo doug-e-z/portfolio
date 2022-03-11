5a8e8508d9ff6892a5f46d144a3cec8ccd8822a8
MSI <75710210+doug-e-z@users.noreply.github.com>
2022-03-10 19:16:19 -0500

    Designed basic form

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,9 +1,54 @@
+import { Col, Container, Row } from "react-bootstrap/";
+import "../static/css/contact.css";
+
 const Contact = () => {
-    return (
-        <div>
-            <h1>Contact</h1>
-        </div>
-    );
+  function onSubmit(event) {
+    alert("Submit button was pushed");
+    event.preventDefault();
+  }
+
+  return (
+    <div>
+      <Container className="container-view">
+        <Row className="form-title border-dark border rounded border-5">
+          <form className="form-view" onSubmit={onSubmit}>
+            <fieldset>
+              <legend>Contact Me</legend>
+              <Col>
+                <div>
+                  <label className="form-label label-view">
+                    Email Address:
+                  </label>
+                  <input
+                    className="form-control"
+                    type="email"
+                    placeholder="name@example.com"
+                  />
+                </div>
+              </Col>
+              <Col>
+                <div>
+                  <label className="form-label label-view">Subject:</label>
+                  <input
+                    className="form-control"
+                    type="text"
+                    placeholder="subject"
+                  />
+                </div>
+              </Col>
+              <Col>
+                <div>
+                  <label className="form-label label-view">Message:</label>
+                  <textarea className="form-control" rows="4"></textarea>
+                </div>
+              </Col>
+              <input className="label-view" type="submit" value="Submit" />
+            </fieldset>
+          </form>
+        </Row>
+      </Container>
+    </div>
+  );
 };
 
 export default Contact;
